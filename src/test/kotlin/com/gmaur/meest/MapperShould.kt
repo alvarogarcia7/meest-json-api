b304fc305bff70d4acee6086e9d1b5940f01893e
Alvaro Garcia <derecerca@gmail.com>
2018-02-27 00:04:04 +0100

    Add JUnit5 dependencies + Green: create dynamic tests

--- src/test/kotlin/com/gmaur/meest/MapperShould.kt
+++ src/test/kotlin/com/gmaur/meest/MapperShould.kt
@@ -5,12 +5,19 @@ import com.gmaur.meest.Meest.Mapper
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
+import org.junit.platform.runner.JUnitPlatform
+import org.junit.runner.RunWith
 
+@RunWith(JUnitPlatform::class) // this is necessary for infinitest
 class MapperShould {
 
-    private lateinit var mapper: Meest.Mapper
+    private var mapper: Meest.Mapper? = null
 
     @Before
+    @BeforeEach
     fun setup() {
         mapper = Mapper()
     }
@@ -20,9 +27,26 @@ class MapperShould {
         val message = "Ошибка авторизации"
         val serviceResponse = createResponse("101", message)
 
-        val response = mapper.map(serviceResponse.toEither())
+        val response = mapper?.map(serviceResponse.toEither())!!
 
         assertResponseIs(response, "Authentication Error", message)
+    }
+
+    @TestFactory
+    fun translateDynamic(): List<DynamicTest> {
+        val testCases = listOf(Triple("101", "Authentication Error", ""))
+        return testCases.map {
+            DynamicTest.dynamicTest(
+                    " Test for translation " + " case ${it.first} == ${it.second}",
+                    {
+                        val message = "sample message"
+                        val serviceResponse = createResponse(it.first, message)
+
+                        val response = mapper?.map(serviceResponse.toEither())!!
+
+                        assertResponseIs(response, it.second, message)
+                    })
+        }
     }
 
     private fun createResponse(code: String, message: String): Response {
