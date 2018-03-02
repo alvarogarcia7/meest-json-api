10c86f7c916d835f9c6711db0fc03ae576f18e6c
Alvaro Garcia <derecerca@gmail.com>
2018-03-02 20:31:42 +0100

    Refactor: rename field

--- src/main/kotlin/com/gmaur/meest/Result.kt
+++ src/main/kotlin/com/gmaur/meest/Result.kt
@@ -9,8 +9,8 @@ data class Branch(val type: String?, val code: String?, val typeCode: String?) {
 data class Result(val city: Triad, val branch: Branch, val district: Triad, val addressMoreInformation: String?, val b2c: String?, val longitude: String?, val latitude: String?,
                   val house: String?, val description: Biad, val limitWeight: String?, val region: Triad, val streetType: Biad, val street: Triad, val id: String?, val stickerCode: String?, val workingHours: String?, val zipCode: String?)
 
-data class Results(val values: List<Result>) {
+data class Results(val droppoints: List<Result>) {
     fun first(): Result {
-        return values.first()
+        return droppoints.first()
     }
 }
